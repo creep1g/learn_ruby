31afec077f836ef91c949cb48dddab09acfe7240
Þorgils Árni Hjálmarsson <creep1g@gmail.com>
2019-02-11 17:22:00 +0000

    solved

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,22 @@
 class Book
-# write your code here
+    # write your code here
+    def title=(name)
+        words = name.split
+        words[0].capitalize!
+        little_words = ['the','a','an','and','in','of','to']
+        words[1..-1].map {|word|
+            if little_words.include?(word)
+                word
+            else
+            word.capitalize!
+            end}
+    @title = words.join(" ")
+end
+
+def title 
+    @title
+end
+
+        
+
 end
