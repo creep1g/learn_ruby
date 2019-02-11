0c0ac32f9af629fdc6057fcd1d1d3eb2a981106b
Þorgils Árni Hjálmarsson <creep1g@gmail.com>
2019-02-11 17:24:45 +0000

    solved

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
 #write your code here
+def add x, y
+    x + y
+end
+
+def subtract x, y
+    x - y
+end
+
+def sum array
+    array.inject(0){|sum,x| sum + x }
+end
+
+def multiply(*numbers)
+    numbers.reduce(:*)
+end 
+
+def power x, y
+    x**y
+end
+
+def factorial x
+    (1..x).inject(:*) || 1
+end
