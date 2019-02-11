ea9455d16852bc51a4d3a8169cb5f4ead23f9b04
Þorgils Árni Hjálmarsson <creep1g@gmail.com>
2019-02-11 17:22:38 +0000

    solved

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,22 @@
 #write your code here
+def translate(str)
+    # vowels = ["a", "e", "i", "o", "u"]
+    # consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "z", "w", "y"]
+
+    words = str.split
+
+    words.each do |word|
+        vowels = ["a", "e", "i", "o", "u"]
+        if vowels.include?(word[0]) ## string starts with vowel
+            word.replace word + "ay"
+      else
+	        vowel = word.index(/[aeio]/)
+			word.replace word[vowel..-1] + word[0..vowel-1] + "ay"
+		end
+    end
+    words.join(" ")
+
+
+end
+
+
