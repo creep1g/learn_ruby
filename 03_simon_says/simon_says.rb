2e562d034952785f686c30bc9233dd1235595e31
Þorgils Árni Hjálmarsson <creep1g@gmail.com>
2019-02-11 17:23:18 +0000

    solved

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
 #write your code here
+def echo string 
+    string
+end
+
+def shout string
+    string.upcase
+end
+
+def repeat(z, y=2)
+    ([z] * y).join(" ")
+end
+
+def start_of_word(word, number)
+    word[0, number]
+end
+
+def first_word string
+    word = string.split(" ")
+    word[0]
+end
+
+def titleize word
+    word.capitalize!
+    words_no_cap = ["and", "or", "the", "over", "to", "the", "a", "but"]
+    phrase = word.split(" ").map {|word| 
+    if words_no_cap.include?(word)
+        word
+    else
+        word.capitalize
+    end
+    }.join(" ")
+    phrase
+end
